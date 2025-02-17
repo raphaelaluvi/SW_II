ede916acfab53367e86942a63b43f7ba17acfd95
raphaelaluvi <raphaluvi@gmail.com>
2025-02-17 11:08:36 -0300

    aula2

--- aula02_17-02/exercicios/ex4.php
+++ aula02_17-02/exercicios/ex4.php
@@ -1,2 +1,30 @@
 <?php
+    echo "Hoje é que dia da semana?";
+    $dia = 2;
+
+    switch ($dia) {
+        case 1:
+            echo "Opção a escolhida!";
+            break;
+        case 2: 
+            echo "Opção b escolhida!";
+            break; //usa p break para na continuar no default
+        case 1:
+            echo "Opção a escolhida!";
+            break;
+        case 1:
+            echo "Opção a escolhida!";
+            break;
+        case 1:
+            echo "Opção a escolhida!";
+            break;   
+        case 1:
+            echo "Opção a escolhida!";
+            break;  
+        case 1:
+            echo "Opção a escolhida!";
+            break;   
+        default:
+            echo "Opção inválida!";
+    }
 ?>
