6a0d9c72329a706220be003115e97dfc7d8fe831
raphaelaluvi <raphaluvi@gmail.com>
2025-02-25 09:43:39 -0300

    teste

--- aula02_17-02/exercicios/ex7.php
+++ aula02_17-02/exercicios/ex7.php
@@ -1,7 +1,18 @@
 <?php
     $nomes = ["Rapha", "Claudia", "Hoender", "Jack"];
+    
+    echo "Feito com foreach: <br>";
 
     foreach ($nomes as $nome) {
         echo $nome . "<br>";
     }
+
+    $quant = count($nomes);
+    echo $quant . "<br>";
+
+    // echo "Feito com for: <br>";
+    // for ($i = 0; $i < 3; $i++){
+    //     echo $nomes[$i]. "<br";
+    // }
+
 ?>
