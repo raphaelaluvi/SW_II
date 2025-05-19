a13fb3b7636893f9d1bfdc60b711c4c86dc274b1
raphaelaluvi <raphaluvi@gmail.com>
2025-05-19 20:19:51 -0300

    apiok

--- aula11_13-05/index.php
+++ aula11_13-05/index.php
@@ -1,11 +1,20 @@
 <?php
-    $cepRecebido = isset($_GET['busca']) ? $_GET['busca'] : '';
+    $cepRecebido = isset($_GET['cepRecebido']) ? $_GET['cepRecebido'] : '';
+    $cep = []; // começa vazio
 
-    $url = "https://viacep.com.br/ws/{$cepRecebido}/json/";
-    $response = file_get_contents($url);
-    $cep = json_decode($response, true);
+    // só chama se tiver valor
+    if (!empty($cepRecebido)) {
+        $url = "https://viacep.com.br/ws/{$cepRecebido}/json/";
+        $response = @file_get_contents($url);
+        if ($response !== false) {
+            $cep = json_decode($response, true);
 
-
+            // Verifica se a API respondeu com erro (CEP inválido)
+            if (isset($cep['erro']) && $cep['erro'] === true) {
+                $cep = []; // limpa para não tentar exibir dados inválidos
+            }
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -14,12 +23,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listagem de CEP</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Consulta de Endereço via CEP</h1>
 
-    <form method="GET" class="form-busca">
-        <input type="text" name="busca" placeholer="Digite o CEP (Ex: 09402060)" value="<?php echo htmlspecialchars($busca);?>">
+    <form method="GET" class="form-cepRecebido">
+        <input type="text" name="cepRecebido" placeholder="Digite o CEP (Ex: 09402060)" inputmode="numeric" pattern="\d{8}" value="<?php echo htmlspecialchars($cepRecebido);?>">
         <button type="submit">Consultar</button>
     </form>
 
@@ -27,15 +37,13 @@
         <?php if (empty($cep)): ?>
             <p>Nenhum CEP encontrado.</p>
         <?php else: ?>
-            <?php foreach ($cep as $cepIndividual): ?>
                 <div class="card">
-                    <p><strong>CEP: <strong><?php echo $cepIndividual['cep']; ?></p>
-                    <p><strong>Logradouro: <strong><?php echo $cepIndividual['logradouro']; ?></p>
-                    <p><strong>Bairro: <strong><?php echo $cepIndividual['bairro']; ?></p>
-                    <p><strong>Cidade: <strong><?php echo $cepIndividual['localidade']; ?></p>
-                    <p><strong>Estado: <strong><?php echo $cepIndividual['estado'] . " - " .$cepIndividual['uf']; ?></p>
+                    <p><strong>CEP: </strong><?php echo $cep['cep']; ?></p>
+                    <p><strong>Logradouro: </strong><?php echo $cep['logradouro']; ?></p>
+                    <p><strong>Bairro: </strong><?php echo $cep['bairro']; ?></p>
+                    <p><strong>Cidade: </strong><?php echo $cep['localidade']; ?></p>
+                    <p><strong>Estado: </strong><?php echo $cep['uf']; ?></p>
                 </div>
-            <?php endforeach; ?>
         <?php endif; ?>
         
     </div>
